1ac5696044e80154bf431171a5770c982223cced
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-04-23 22:14:26 +0300

    pie chart added to home page

--- src/components/AnaSayfa.js
+++ src/components/AnaSayfa.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 import '../styles/AnaSayfa.css';
+import PieChart from "recharts/es6/chart/PieChart";
+import Pie from "recharts/es6/polar/Pie";
+import Tooltip from "recharts/es6/component/Tooltip";
+
+const data = [{name: 'Araba', value: 400}, {name: 'Telefon', value: 300},
+    {name: 'Yiyecek', value: 300}, {name: 'Bilgisayar', value: 200},
+    {name: 'Giyecek', value: 278}, {name: 'Yedek parça', value: 189}]
+
 
 export default class AnaSayfa extends React.Component {
     render() {
         return(
-            <h2 style={{background: "#a2ff4c"}}>Ana Sayfa</h2>
+            <div className={"flex-center"}>
+                <h2 className={"text-center"}>Ana Sayfa</h2>
+
+                <div>
+                    <PieChart width={400} height={400}>
+                        <Pie isAnimationActive={true} data={data} cx={200} cy={200} outerRadius={140} fill="#0aa2a8"
+                             label/>
+                        <Tooltip/>
+                    </PieChart>
+                </div>
+            </div>
         );
     }
 }
