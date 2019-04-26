8863d6aa938284f3b67f2eadd8bbde93b952fa48
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-04-26 22:01:10 +0300

    HareketDokumu component template done

--- src/components/HareketDokumu.js
+++ src/components/HareketDokumu.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-import '../styles/HareketDokumu.css';
+const styles = theme => ({
+    root: {
+        width: '85%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        maxWidth: 900,
+    },
+});
 
-export default class HareketDokumu extends React.Component {
+let id = 0;
+
+function createData(name, calories, fat, carbs, protein) {
+    id += 1;
+    return {id, name, calories, fat, carbs, protein};
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+class HareketDokumTablo extends React.Component {
     render() {
-        return(
-            <h2 style={{background: "#a2ff4c"}}>Hareket Dökümü</h2>
+        return (
+            <div>
+                <h2>Hareket Dökümü</h2>
+                <Paper className={this.props.classes.root}>
+                    <Table className={this.props.classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell>Calories</TableCell>
+                                <TableCell>Fat (g)</TableCell>
+                                <TableCell>Carbs (g)</TableCell>
+                                <TableCell>Protein (g)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map(row => (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell>{row.calories}</TableCell>
+                                    <TableCell>{row.fat}</TableCell>
+                                    <TableCell>{row.carbs}</TableCell>
+                                    <TableCell>{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+
+            </div>
         );
     }
+
+
 }
+
+export default withStyles(styles)(HareketDokumTablo);
